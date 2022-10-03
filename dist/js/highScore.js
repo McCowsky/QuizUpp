7c90700e5fee853d00440ec5b8213544501e2cea
McCowsky <makowskimateusz@outlook.com>
2022-10-03 22:31:32 +0200

    fix typos

--- dist/js/highScore.js
+++ dist/js/highScore.js
@@ -1,7 +1,6 @@
 "use strict";
 const scoreListBox = document.getElementById("scoreListBox");
 const highscores1 = JSON.parse(localStorage.getItem("highscores") || "[]");
-console.log(highscores1);
 if (scoreListBox != null) {
     scoreListBox.innerHTML = highscores1
         .map((element) => {
--- src/ts/highScore.ts
+++ src/ts/highScore.ts
@@ -2,7 +2,6 @@ const scoreListBox = document.getElementById(
   "scoreListBox"
 ) as HTMLUListElement | null;
 const highscores1 = JSON.parse(localStorage.getItem("highscores") || "[]");
-console.log(highscores1);
 
 if (scoreListBox != null) {
   scoreListBox.innerHTML = highscores1
